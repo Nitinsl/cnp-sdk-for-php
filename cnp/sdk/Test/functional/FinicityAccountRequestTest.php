0e8d9da3f9f933cb85416e773d77936ee3551830
Nitin Lingayat <nlingayat@nufl2rdd00747.vapdev.int>
2024-07-17 07:56:38 -0400

    commiting test changes

---
+++ cnp/sdk/Test/functional/FinicityAccountRequestTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace cnp\sdk\Test\functional;
+
+use cnp\sdk\CnpOnlineRequest;
+use cnp\sdk\CommManager;
+use cnp\sdk\XmlParser;
+
+class finicityAccountRequestTest extends \PHPUnit_Framework_TestCase
+{
+    public static function setUpBeforeClass()
+    {
+        CommManager::reset();
+    }
+
+    public function test_simple_finicity_Account_Request()
+    {
+        $hash_in = array('id' => 'id',
+            'id' => '1211',
+            'reportGroup' => 'Planets',
+            'echeckCustomerId' => 'abcdef'
+        );
+        $initialize = new CnpOnlineRequest();
+        $finicityAccountResponse = $initialize->finicityAccountRequest($hash_in);
+        $response = XmlParser::getNode($finicityAccountResponse, 'response');
+        $this->assertEquals('000', $response);
+        $location = XmlParser::getNode($finicityAccountResponse, 'location');
+        $this->assertEquals('sandbox', $location);
+
+    }
+
+}
